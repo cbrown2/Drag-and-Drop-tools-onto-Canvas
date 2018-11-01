d8608793f5d76b2135d2c3ca4f925cd51a0b5acd
cbrown2 <43274952+cbrown2@users.noreply.github.com>
2018-11-01 10:47:25 -0400

    Add files via upload

---
+++ drag and drop/main.js
@@ -0,0 +1,33 @@
+$(init);
+
+function init() {
+    //drag
+    $(".tool").draggable({
+        helper: "clone"
+    });
+    //Drop
+    $(".canvas").droppable({
+    drop:(event, ui)=> {
+        // console.log(ui.position);
+        // console.log(ui.draggable[0].innerHTML);
+        var node= {
+            position:ui.position,
+            html:ui.draggable[0].innerHTML
+        };
+        if(ui.helper.hasClass("tool")){
+        render(node);
+    }
+    }
+    });
+    
+}
+function render (node) {
+var html = "<h3>" + node.html +"</h3>";
+var dom = $(html).css({
+    position: "absolute",
+    top: node.position.top,
+    left: (node.position.left-= $(".canvas").position().left)
+
+});
+$(".canvas").append(dom.draggable());
+}
